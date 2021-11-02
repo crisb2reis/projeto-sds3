7d4b5ef0de339a2786db34def26bc6b2fbe32b9a
Cristiano Reis <crisb2reis@hotmail.com>
2021-11-02 20:45:40 -0300

    Criando projeto

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,23 @@
+// function NavBar() {
+//     return (
+//         <div >
+//             <h2>Dev Superior</h2>
+//         </div>
+//     );
+// }
+// export default NavBar;
+const Footer = () => {
+    return (
+        <div >
+            <footer className="footer mt-auto py-3 bg-dark">
+                <div className="container">
+                    <p className="text-light">App desenvolvido por <a href="https://github.com/acenelio" target="_blank" rel="noreferrer">Cristiano Reis</a></p>
+                    <p className="text-light"><small><strong>Semana Spring React</strong><br />
+      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/crisb2reis" target="_blank" rel="noreferrer">@crisb2reis</a></small></p>
+                </div>
+            </footer>
+        </div>
+    );
+}
+
+export default Footer;
